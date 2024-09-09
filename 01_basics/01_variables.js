1dcd6d20720a4a2d955be93ad36d7a466fbfd9c4
GhostBYte <123203175+sujalnitk@users.noreply.github.com>
2024-09-09 07:46:54 +0000

    added variable basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountPass = "123"
+var acccountCity = "Jaipur"
+accountEmail = "sujal@gmail.com"
+
+/*
+let is used as var has scope problem
+
+How to write in a cmd line 
+node filename
+*/
+
+
+console.table([accountId , acccountCity , accountPass , accountEmail])
