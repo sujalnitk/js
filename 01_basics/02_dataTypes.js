d53af8c473aae2ec714f503a59b08994696e49af
GhostBYte <123203175+sujalnitk@users.noreply.github.com>
2024-10-09 20:08:08 +0000

    added datatypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" ;// treat all JS code as newer version
+// alert(3+3) // we are using nodejs not browser 
+console.log(3+3
+) // code readability
+
+let name = "Sujal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string 
+// null => standalone value - object 
+// undefined => value not assigned
+// symbol => unique 
+
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
