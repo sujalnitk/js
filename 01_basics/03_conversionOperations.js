7b10ae855ccc43c415cb037a3267d5c5dc0ac222
GhostBYte <123203175+sujalnitk@users.noreply.github.com>
2024-10-09 20:32:40 +0000

    added 03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "a"
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+console.log("type of score" , typeof(score));
+
+// 1 is true for boolean 
+// empty string is false in boolean
+// something written in string is true in boolean
+// when converted 
